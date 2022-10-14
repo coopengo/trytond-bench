fbdb4faa45af658dd1df786dcd910919c9c7c3fe
Vincent Garde <67907585+CoopVincent@users.noreply.github.com>
2022-10-14 16:08:04 +0200

    bench: Update tests to Tryton 6.4 (#9)
    Fix #00000

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -1,8 +1,5 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import unittest
-
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
@@ -11,8 +8,4 @@ class BenchTestCase(ModuleTestCase):
     module = 'bench'
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            BenchTestCase))
-    return suite
+del ModuleTestCase
