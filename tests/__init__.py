7d04accd7a6d5ba4e4e1ffce959a0c0caba03acf
Vincent Garde <vincent.garde@coopengo.com>
2022-10-14 11:48:11 +0200

    merge_6_4

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,2 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-try:
-    from trytond.modules.bench.tests.test_bench import suite
-except ImportError:
-    from .bench import suite
-
-__all__ = ['suite']
